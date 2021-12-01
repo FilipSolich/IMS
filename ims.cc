18a7e348e3896ca8f30a1f2e70dd9c8317a90237
Filip Solich <filip.solich@tuta.io>
2021-12-01 17:59:18 +0100

    Simlib structure

--- ims.cc
+++ ims.cc
@@ -1,6 +1,24 @@
+/*
+ * IMS
+ *
+ * Date:    30. 11. 2021
+ * Authors: Filip Solich <xsolic00@stud.fit.vutbr.cz>
+ *          Marek Sechra <xsechr00@stud.fit.vutbr.cz>
+ */
+
 #include <iostream>
 
+#include <stdlib.h>
+
+#include <simlib.h>
+
+
+const double T0 = 0.0;
+const double T1 = 100.0; // TODO: change
 
 int main() {
-    std::cout << "test" << std::endl;
+	Init(T0, T1);
+	Run();
+
+	return EXIT_SUCCESS;
 }
